1331374ab5778152afae65360fdad15e1afcb697
knightchaser <agerio100@naver.com>
2025-11-22 00:27:34 +0900

    Ci: Prepare GitHub pages CI/CD configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,18 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: '/chrono-crunch/', // For GitHub Pages configuration
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+  };
 });
