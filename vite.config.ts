49d0fcced8e16a4931a420dbde05588393e42e21
knightchaser <agerio100@naver.com>
2025-11-22 00:39:26 +0900

    Fix: Correct vite base to match with the real repository name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/chrono-crunch/', // For GitHub Pages configuration
+    base: '/chronocrunch/', // For GitHub Pages configuration
     server: {
       port: 3000,
       host: '0.0.0.0',
